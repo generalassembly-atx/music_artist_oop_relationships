fd87e2f16958db9c43e847d1516f144956422734
Chris Robbins <Jaguar85>
2016-08-11 22:07:22 -0500

    half way point

--- music.js
+++ music.js
@@ -1,15 +1,25 @@
 // Define the Artist constructor function here, with a 'name' property that can be set at instantiation,
 // and a 'songs' property that is an empty array to start
-function Artist() {
+function Artist(name, songs) {
+  this.name = name,
+  this.songs = []
 
 }
 
 // Define the Song constructor function here, with 'title' and 'artist' properties that can be set at instantiation,
 // and a 'playCount' property that is an empty array to start
-function Song() {
-
+function Song(title, artist, playcount) {
+  this.title = "Jump!",
+  this.artist = VanHalen,
+  this.playcount = [0]
+}
   // When a new song is instantiated, you'll want to add that song to its artist's 'songs' array.
   // You'll probably want to do that in here somewhere...
+  Song.prototype.play = function () {
+  for (var i = 0; i < this.playCount.length; i++) {
+    this.playCount[i]
+  }
+
 }
 
 // This is how we should be able to use your constructor functions once they work!
