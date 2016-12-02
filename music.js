21edf3742d0c5eac844f89e0876ff1ea0b451308
Paul Lizama <plizama49@gmail.com>
2016-12-01 23:14:19 -0600

    oop_hw

--- music.js
+++ music.js
@@ -1,17 +1,62 @@
 // Define the Artist constructor function here, with a 'name' property that can be set at instantiation,
 // and a 'songs' property that is an empty array to start
-function Artist() {
+
+function Artist(name, songs) {
+  this.name = name;
+  this.songs = [];
 
 }
 
 // Define the Song constructor function here, with 'title' and 'artist' properties that can be set at instantiation,
 // and a 'playCount' property that is set to 0
-function Song() {
 
+function Song(title, artist) {
+  this.title = title;
+  this.artist = artist;
+  this.playcount = 0;
+
+  (this.artist).songs.push(this.title);
+  console.log(vanHalen.songs);
   // When a new song is instantiated, you'll want to add that song to its artist's 'songs' array.
   // You'll probably want to do that in here somewhere...
 }
 
+
+
+
+
+
+
 // This is how we should be able to use your constructor functions once they work!
 var vanHalen = new Artist("Van Halen");
 var jump = new Song("Jump!", vanHalen);
+
+
+
+
+
+
+
+
+// REFERENCE
+
+// Function example w 'this'
+//
+// function expression
+//
+// function Animal(name, species, call){
+//   this.name = name;
+//   this.species = species;
+//   this.wildCall = call;
+//
+//   this.explainYourself = function(){
+//     console.log(
+//       ["Hi! i'm ", this.name, ' the ', this.species].join('')
+//       );
+//
+//   };
+// };
+//
+// var rabbit = new Animal('Roger', 'Rabbit','shirp grunnnnnnt');
+//
+// rabbit.explainYourself();
